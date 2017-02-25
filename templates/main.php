75f45fc630a82a54c6c20f761eeb0038e62bc08f
kurraz <elfd01@gmail.com>
2017-02-25 14:51:19 +0200

    add db scheme dump

--- templates/main.php
+++ templates/main.php
@@ -14,62 +14,7 @@
         }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script>
-        function load_queries()
-        {
-            $('#block-queries').hide();
-            $('#block-queries-loading-img').show();
-
-            $.ajax({
-                url: '/?r=get-queries',
-                success: function(data){
-                    $('#block-queries').html(data);
-                    $('#block-queries').show();
-                    $('#block-queries-loading-img').hide();
-                }
-            })
-        }
-
-        $(function(){
-            load_queries();
-
-            $('#block-queries').on('click','.link-query',function(e){
-                e.preventDefault();
-
-                $('#block-summary-loading-img,#block-results-loading-img').show();
-                $('#block-summary,#block-results').hide();
-
-                $.ajax({
-                    url: $(this).attr('href'),
-                    dataType: 'json',
-                    success: function(data){
-                        $('#block-summary-name').text(data['summary']['name']);
-                        $('#block-summary-cnt').text(data['summary']['cnt']);
-                        $('#block-summary-avg-rating').text(data['summary']['avg_rating']);
-
-                        $('#block-results').html(data['results']);
-
-                        $('#block-summary-loading-img,#block-results-loading-img').hide();
-                        $('#block-summary,#block-results').show();
-                    }
-                });
-            });
-
-            $('#form-add-query').submit(function(e){
-                e.preventDefault();
-
-                var data = $(this).serialize();
-                $.ajax({
-                    url: $(this).attr('action'),
-                    type: 'post',
-                    data: data,
-                    success: function(data){
-                        load_queries();
-                    }
-                });
-            });
-        })
-    </script>
+    <script src="script.js"></script>
 </head>
 <body>
     <div class="main">
